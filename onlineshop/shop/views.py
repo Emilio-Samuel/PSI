41fd4ad2a9f2f5b4dc8b1d0e14a795d34c241c59
eaced <samuelemil@hotmail.com>
2017-11-14 18:13:17 +0100

    Views implementadas

--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -6,6 +6,32 @@ from django.shortcuts import render
 from django.views import generic
 from shop.models import Product,Category
 from django.http import Http404
+
+def product_list(request, catSlug=None):
+	#Your code goes here
+	#queries that fill, category, categories and products
+
+	categories=Category.objects.all()
+
+	if catSlug == None:
+		category = None
+		products = Product.objects.all()
+	else:
+		category = Category.objects.get(Category.catSlug=catSlug)
+		products = Product.objects.get(Category.catSlug=catSlug)
+		
+
+	return render(request,'shop/list.html', {'category': category, 'categories':categories, 'products': products})
+
+
+def detailProduct(request, prodId, prodSlug):
+	#Your code goes here
+	#query that returns a product with id=protId
+
+	product = Product.objects.get(pk=prodId)
+	return render(request, ’shop/detail.html’, {’product’: product})
+	
+
 def index(request):
 	category_list = Category.objects.all()
 	product_list = Product.objects.all()
